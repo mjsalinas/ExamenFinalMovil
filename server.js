b6f393061a1c265a95a1b209835f2a01090a2ecc
DESKTOP-CKUTKR2\Hector Santos <hectorsantoslopez6@gmail.com>
2025-06-27 19:07:17 -0600

    Package

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/reviews', reviewRoutes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+const PORT = process.env.PORT || 3001;
+app.listen(
+    PORT, 
+    ()=>console.log(`Servidor corriendo en puerto ${PORT}`));
