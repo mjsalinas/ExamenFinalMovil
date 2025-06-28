9f0d8efe66ce90307715e32f207fcad8dbe130de
AbrahamRP97 <abrahamj.rivera97@gmail.com>
2025-06-27 18:42:13 -0600

    Correciones en server.js / Creacion de supabaseClient

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dotenv = require('dotenv');
+
 const reviewRoutes = require('./routes/reviewRoutes');
+
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+
 app.use('/reviews', reviewRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
