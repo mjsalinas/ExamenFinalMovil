5f241f117ef58963653cfdacca379f8bcc183e45
DESKTOP-CKUTKR2\Hector Santos <hectorsantoslopez6@gmail.com>
2025-06-27 18:26:35 -0600

    k

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const reviewRoutes = require('./routes/reviewRoutes');
 //Examen Hector Santos
-//Hola Mundo
+
 app.use(cors());
 app.use(express.json());
 app.use('/reviews', reviewRoutes);
