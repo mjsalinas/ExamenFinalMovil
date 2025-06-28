72d71cfddf62f9ddde6e5e510825d155b77d5b0e
williamjosue782 <williamjosue782@gmail.com>
2025-06-28 00:24:52 -0600

    mensaje

---
+++ controllers/config/supabaseClient.js
@@ -0,0 +1,10 @@
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('SUPABASE_URL y SUPABASE_KEY deben estar en .env');
+}
+
+module.exports = createClient(supabaseUrl, supabaseKey);
